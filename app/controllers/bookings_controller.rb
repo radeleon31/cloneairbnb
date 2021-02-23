789e1dd54de31e808ccecbb413bb041ddf0323ff
juan Illingworth <jpillingworth@hotmail.com>
2021-02-23 15:48:26 -0300

    booking routes and actions (views missing)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
+  
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @helicopter = Helicopter.find(params[:helicopter_id])
+    @booking.user = current_user
+    @booking.helicopter = @helicopter
+    if booking.save
+    redirect_to booking_path(@booking)    
+    else
+      redirect_to helicopter_path(@helicopter)
+    end
+  end
+  
+  def index
+    @bookings = Booking.all
+  end
+    
+  def show
+    @booking = Booking.find(params[])
+  end
+    
+  def destroy
+    @booking = Booking.find(params[])
+    @booking.destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:helicopter_id, :user_id)
+  end
 end

69b4062621d96ba9dcb4c91b85f03377728aa687
Maxi Amodei <maxi_amodei@hotmail.com>
2021-02-23 14:40:58 -0300

    Helicopter new created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :helicopter do
-    resources :booking, only: [:new, :create]
+  resources :helicopters do
+    resources :bookings, only: [:new, :create]
   end
-    resources :booking, only: [:show, :destroy, :index]
+    resources :bookings, only: [:show, :destroy, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
